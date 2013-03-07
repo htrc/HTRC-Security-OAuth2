21f1565b08ab20c9fd9ffc9e3782b7031ea855a8
Milinda Pathirage <milinda.pathirage@gmail.com>
2013-03-07 17:51:34 +0000

    deleting unwated wso2 module sources.

--- modules/wso2is-extensions/org.wso2.carbon.identity.provider/4.0.3/src/main/java/org/wso2/carbon/identity/provider/openid/OpenIDServerConstants.java
+++
@@ -1,7 +0,0 @@
-package org.wso2.carbon.identity.provider.openid;
-
-public class OpenIDServerConstants {
-	
-	public static final String ASSOCIATION_STORE_TYPE_SHARED = "SHARED";
-	public static final String ASSOCIATION_STORE_TYPE_PRIVATE = "PRIVATE";
-}
