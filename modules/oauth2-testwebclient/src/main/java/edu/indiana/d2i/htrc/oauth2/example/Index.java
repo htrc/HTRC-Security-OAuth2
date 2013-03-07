66ed5bedc8ffd99713f61d106ee48186ba26681f
Milinda Pathirage <milinda.pathirage@gmail.com>
2013-03-07 17:15:58 +0000

    Deleting half baked sample code.

--- modules/oauth2-testwebclient/src/main/java/edu/indiana/d2i/htrc/oauth2/example/Index.java
+++
@@ -1,9 +0,0 @@
-package edu.indiana.d2i.htrc.oauth2.example;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class Index extends ActionSupport {
-    public String execute() throws Exception{
-        return LOGIN;
-    }
-}
